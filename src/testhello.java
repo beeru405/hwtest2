5b01cbebeae1885590dae287e30d5bc1b2e9e673
beeru405 <101712802+beeru405@users.noreply.github.com>
2023-03-01 10:21:48 +0530

    Create testhello.java

---
+++ src/testhello.java
@@ -0,0 +1,16 @@
+package com.aks;
+public class Test1{
+  public static void main(String[] args){
+  
+  String results =Test.m1();
+  System.out.println(results);
+  String[] res=result.split("");
+  if(res[0].equals("Hello")){
+  
+  System.out.println("Test Passed");
+  }
+  else {
+   System.out.println("Test Failed");
+  }
+  }
+}
